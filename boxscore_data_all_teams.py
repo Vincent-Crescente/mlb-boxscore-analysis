857cf731e895f2e968b7fe978344177b3c6157a7
Vincent Crescente <vcrescente.gmail.com>
2021-07-25 16:54:48 -0400

    cleaning comments

--- boxscore_data_all_teams.py
+++ boxscore_data_all_teams.py
@@ -7,6 +7,7 @@ import pandas as pd
 from collections import defaultdict
 from teams_and_ids import teams_and_ids
 
+parser = argparse.ArgumentParser(description="****Creates a csv file in path given displaying amount of runs for each inning every game inbetween the dates****")
 
 def boxscore_data(team_name, schedule_obj):
 
@@ -18,21 +19,14 @@ def boxscore_data(team_name, schedule_obj):
             if not season_linescore_dict[x['game_id']]:
                 season_linescore_dict[x['game_id']].append(x['game_date'])
                 season_linescore_dict[x['game_id']].append(team_name)
-                # print("G_ID:", x['game_id'])
-                # print("Date: ", x['game_date'])
-                # print("Main Team", team_n
 
                 if x['away_name'] == team_name:
-                    # print("Home Name:", x['home_name'])
                     season_linescore_dict[x['game_id']].append(x['home_name'])
                     season_linescore_dict[x['game_id']].append("N")
                 else:
-                    # print("Away Name:", x['away_name'])
                     season_linescore_dict[x['game_id']].append(x['away_name'])
                     season_linescore_dict[x['game_id']].append("Y")
 
-                # print("Double Header:", x['doubleheader'])
-                # print("----------------------------")
                 season_linescore_dict[x['game_id']].append(x['doubleheader'])
             elif KeyError:
                 continue
@@ -54,8 +48,6 @@ def boxscore_data(team_name, schedule_obj):
             except KeyError:
                 season_linescore_dict[game_id].append(0)
 
-    # print(season_linescore_dict)
-    # print(max_inning)
     max_team_inning_played = [str(x) for x in range(1, max_inning+1)]
     season_linescore = pd.DataFrame.from_dict(season_linescore_dict, columns=["date", "team", "opp", "H", "DBH"] + max_team_inning_played,  orient='index')
 
@@ -65,20 +57,27 @@ def boxscore_data(team_name, schedule_obj):
     return season_linescore
 
 
-#
-# try:
+parser.add_argument("start_date",
+                    help="Put in Quotes. Format \"MM/DD/YYYY\". Beginning date for boxscore")
+parser.add_argument("end_date",
+                    help="Put in Quotes. Format \"MM/DD/YYYY\". Ending date for boxscore")
+parser.add_argument("folder_path",
+                    help="Put in Quotes. Format \"Desktop\docuement\example\". Ending date for boxscore")
+args = parser.parse_args()
 
-final_df_lists = []
+try:
 
-for team, team_id in teams_and_ids.items():
-    schedule = statsapi.schedule(start_date="04/01/2021", end_date="07/18/2021", team=team_id)
-    temp_df = boxscore_data(team, schedule)
-    final_df_lists.append(temp_df)
-    print(team)
+    final_df_lists = []
 
-final_df = pd.concat(final_df_lists)
-final_df = final_df.fillna(0)
-final_df.to_excel(f"all_mlb_teams.xlsx")
+    for team, team_id in teams_and_ids.items():
+        schedule = statsapi.schedule(start_date=args.start_date, end_date=args.end_date, team=team_id)
+        temp_df = boxscore_data(team, schedule)
+        final_df_lists.append(temp_df)
+        print(team)
 
-# except:
-#     print("Error with parameters according to the data service. Please use command 'python boxscore_data_xlsx_one_team.py -h' for more insight on the parameter and function use.")
+    final_df = pd.concat(final_df_lists)
+    final_df = final_df.fillna(0)
+    final_df.to_excel(args.folder_path + f"all_mlb_teams.xlsx")
+
+except:
+    print("Error with parameters according to the data service. Please use command 'python boxscore_data_xlsx_one_team.py -h' for more insight on the parameter and function use.")
--- boxscore_data_xlsx_one_team.py
+++ boxscore_data_xlsx_one_team.py
@@ -8,10 +8,9 @@ import pandas as pd
 from collections import defaultdict
 from teams_and_ids import teams_and_ids
 
-parser = argparse.ArgumentParser(description="****Creates a csv file in current directory displaying amount of runs for each inning every game inbetween the dates****")
+parser = argparse.ArgumentParser(description="****Creates a csv file in path given displaying amount of runs for each inning every game inbetween the dates****")
 
-
-def boxscore_data(team_name, schedule_obj):
+def boxscore_data(team_name, schedule_obj, folder_path):
 
     season_linescore_dict = defaultdict(list)
 
@@ -22,21 +21,14 @@ def boxscore_data(team_name, schedule_obj):
             if not season_linescore_dict[x['game_id']]:
                 season_linescore_dict[x['game_id']].append(x['game_date'])
                 season_linescore_dict[x['game_id']].append(team_name)
-                # print("G_ID:", x['game_id'])
-                # print("Date: ", x['game_date'])
-                # print("Main Team", team_n
 
                 if x['away_name'] == team_name:
-                    # print("Home Name:", x['home_name'])
                     season_linescore_dict[x['game_id']].append(x['home_name'])
                     season_linescore_dict[x['game_id']].append("N")
                 else:
-                    # print("Away Name:", x['away_name'])
                     season_linescore_dict[x['game_id']].append(x['away_name'])
                     season_linescore_dict[x['game_id']].append("Y")
 
-                # print("Double Header:", x['doubleheader'])
-                # print("----------------------------")
                 season_linescore_dict[x['game_id']].append(x['doubleheader'])
             elif KeyError:
                 continue
@@ -59,31 +51,29 @@ def boxscore_data(team_name, schedule_obj):
             except KeyError:
                 season_linescore_dict[game_id].append(0)
 
-    # print(season_linescore_dict)
     max_team_inning_played = [str(x) for x in range(1, max_inning+1)]
-    # print(max_team_inning_played)
     season_linescore = pd.DataFrame.from_dict(season_linescore_dict, columns=["date", "team", "opp", "H", "DBH"] + max_team_inning_played,  orient='index')
     season_linescore.index.name = "game_id"
     season_linescore = season_linescore.fillna(0)
-    season_linescore.to_excel(f"season_runs_per_inning_{team_name}.xlsx")
+    season_linescore.to_excel(folder_path + f"season_runs_per_inning_{team_name}.xlsx")
     print("Done")
 
-#
-# parser.add_argument("team",
-#                     help="Put in Quotes. Type fullname with capitals. Ex \"Detroit Tigers\", \"New York Mets\", \"Los Angeles Angels\"")
-# parser.add_argument("start_date",
-#                     help="Put in Quotes. Format \"MM/DD/YYYY\". Beginning date for boxscore")
-# parser.add_argument("end_date",
-#                     help="Put in Quotes. Format \"MM/DD/YYYY\". Ending date for boxscore")
-# args = parser.parse_args()
-#
-# try:
 
-    # schedule = statsapi.schedule(start_date=args.start_date, end_date=args.end_date, team=teams_and_ids.get(args.team))
-    # boxscore_data(args.team, schedule)
+parser.add_argument("team",
+                    help="Put in Quotes. Type fullname with capitals. Ex \"Detroit Tigers\", \"New York Mets\", \"Los Angeles Angels\"")
+parser.add_argument("start_date",
+                    help="Put in Quotes. Format \"MM/DD/YYYY\". Beginning date for boxscore")
+parser.add_argument("end_date",
+                    help="Put in Quotes. Format \"MM/DD/YYYY\". Ending date for boxscore")
+parser.add_argument("folder_path",
+                    help="Path to a folder to save the xlsx file.")
 
-schedule = statsapi.schedule(start_date="04/01/2021", end_date="07/18/2021", team=109)
-boxscore_data("Arizona Diamondbacks", schedule)
+args = parser.parse_args()
 
-# except:
-#     print("Error with parameters according to the data service. Please use command 'python boxscore_data_xlsx_one_team.py -h' for more insight on the parameter and function use.")
+try:
+
+    schedule = statsapi.schedule(start_date=args.start_date, end_date=args.end_date, team=teams_and_ids.get(args.team))
+    boxscore_data(args.team, schedule, args.folder_path)
+
+except:
+    print("Error with parameters according to the data service. Please use command 'python boxscore_data_xlsx_one_team.py -h' for more insight on the parameter and function use.")
